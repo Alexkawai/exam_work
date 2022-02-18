eae9da19ea1e0d52117beed4804723c8530dff40
Alexkawai <leha.iuganov@yandex.ru>
2022-02-18 15:44:53 +0300

    3 files modified, 8 added

---
+++ src/CardContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function CardContent({title, body, state, createdAt, closedAt}){
+    return (<div className={"CardContent " + state}>
+        <h3>{title}</h3>
+        <p className="body">{body}</p>
+        <p className="dates">Создано: {createdAt}</p>
+        {closedAt && <p className="dates">Закрыто: {closedAt}</p>}
+    </div>)}
